944ae90019b6e97de8fe63f4aac08d99cba1ce85
WickedDonutsApp <wickeddonutsales@gmail.com>
2025-11-19 17:46:54 -0800

    Configure Railway to use backend folder

---
+++ WeareWickedSharedTests/WeareWickedSharedTests.swift
@@ -0,0 +1,17 @@
+//
+//  WeareWickedSharedTests.swift
+//  WeareWickedSharedTests
+//
+//  Created by Peter Hesz on 11/9/25.
+//
+
+import Testing
+@testable import WeareWickedShared
+
+struct WeareWickedSharedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
